872ea3da2a36f0755dc30fcf6c9bba815b2d6fda
Joelcio Facanha <joelciomoreira@gmail.com>
2021-03-16 11:43:15 -0300

    Obtendo um post por id

---
+++ src/main/java/com/treinamento/workshopmongo/services/PostService.java
@@ -0,0 +1,23 @@
+package com.treinamento.workshopmongo.services;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.treinamento.workshopmongo.domain.Post;
+import com.treinamento.workshopmongo.repositories.PostRepository;
+import com.treinamento.workshopmongo.services.exceptions.ObjectNotFoundException;
+
+@Service
+public class PostService {
+
+	@Autowired
+	private PostRepository repository;
+
+	public Post findById(String id) {
+		Optional<Post> obj = repository.findById(id);
+		return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
+	}
+
+}
