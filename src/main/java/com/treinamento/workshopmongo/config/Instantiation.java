b7c2b93bf9fc3a3c29a8c99c933f3c365df0174b
Joelcio Facanha <joelciomoreira@gmail.com>
2021-03-16 14:26:43 -0300

    Consulta com vários critérios

--- src/main/java/com/treinamento/workshopmongo/config/Instantiation.java
+++ src/main/java/com/treinamento/workshopmongo/config/Instantiation.java
@@ -39,13 +39,13 @@ public class Instantiation implements CommandLineRunner {
 
 		userRepository.saveAll(Arrays.asList(maria, alex, bob));
 
-		Post post1 = new Post(null, sdf.parse("21/03/2021"), "Partiu Viagem", "Vou viajar para São Paulo. Abraços!",
+		Post post1 = new Post(null, sdf.parse("05/03/2021"), "Partiu Viagem", "Vou viajar para São Paulo. Abraços!",
 				new AuthorDTO(maria));
-		Post post2 = new Post(null, sdf.parse("21/02/2021"), "Bom dia", "Acordei feliz hoje!", new AuthorDTO(maria));
+		Post post2 = new Post(null, sdf.parse("05/02/2021"), "Bom dia", "Acordei feliz hoje!", new AuthorDTO(maria));
 
-		CommentDTO c1 = new CommentDTO("Boa viagem mano!", sdf.parse("21/03/2021"), new AuthorDTO(alex));
-		CommentDTO c2 = new CommentDTO("Aproveite!", sdf.parse("22/03/2021"), new AuthorDTO(bob));
-		CommentDTO c3 = new CommentDTO("Tenha um ótimo dia!", sdf.parse("23/03/2021"), new AuthorDTO(alex));
+		CommentDTO c1 = new CommentDTO("Boa viagem mano!", sdf.parse("05/03/2021"), new AuthorDTO(alex));
+		CommentDTO c2 = new CommentDTO("Aproveite!", sdf.parse("06/03/2021"), new AuthorDTO(bob));
+		CommentDTO c3 = new CommentDTO("Tenha um ótimo dia!", sdf.parse("10/03/2021"), new AuthorDTO(alex));
 
 		post1.getComments().addAll(Arrays.asList(c1, c2));
 		post2.getComments().add(c3);
